8f4cf441903d6de5470d9009ff9f94712686504b
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-03-27 15:14:22 +0530

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/header/header'
+import Footer from './components/footer/footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
 
       <div>
       <Header />
+      <Footer />
       </div>
    
   )

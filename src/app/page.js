1a1185725706840a120f71a8769e9562f3a51aaa
ManzarBilal <manzarbilal@gmail.com>
2023-03-27 15:15:48 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,15 @@ import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/header/header'
+import Search from './components/search/search'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
 
-      <div>
+      <div >
       <Header />
+      <Search/>
       </div>
    
   )

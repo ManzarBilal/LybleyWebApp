2d4066cd69a8aba0f508896154bb80476df5dc7c
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-03-29 14:23:52 +0530

    product

--- src/pages/productDescription/index.js
+++ src/pages/productDescription/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.css"
 import { useState } from 'react'
+import Cards from '../components/cards/cards'
+import Footer from '../components/footer/footer'
+import Header from '../components/header/header'
 
  
  const ProductDescription = () => {
@@ -11,18 +14,22 @@ import { useState } from 'react'
 "https://5.imimg.com/data5/WG/PR/MT/SELLER-7700072/ashok-leyland-spare-part-500x500.png"
 ])
   return (
+    <>
+    <Header />
     <div className='container'>
           <div>
              <img src='https://images.jdmagicbox.com/quickquotes/images_main/imlvo8wloe-148846219-g70if.jpg' alt='3D image' height="150" width="200" />
           </div>
           <div className='row'>
             {imageP?.map((item,i)=>
-            <div className='col-2' key={i}>
-              <img src={item} alt='3D image' height="150" width="200" />
+            <div  className='col-md-3 col-6 d-flex justify-content-center mb-3' key={i}>
+              <Cards img={item} title="cooler" brand={true}/>
             </div>
              )}
           </div>
     </div>
+    <Footer />
+    </>
   )
 }
 export default ProductDescription;

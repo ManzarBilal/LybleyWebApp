c420e9d485680f15c24539ff7077a45dc61692eb
ManzarBilal <manzarbilal@gmail.com>
2023-03-31 13:52:38 +0530

    second commit

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,8 @@
+import changeValue from "./increOrDecre";
 const { combineReducers } = require("redux");
 
 const rootReducer=combineReducers({
-
+      value: changeValue,
 })
 
 export default rootReducer;

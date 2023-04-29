112d679c6bda7c90fdaeef75e6070c6194368a70
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-04-29 18:01:52 +0530

    bhhg

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -57,35 +57,35 @@ BootstrapDialogTitle.propTypes = {
 
 const Checkout = () => {
   const [open, setOpen] = React.useState(false);
-    const data = useSelector(state=>state.checkoutData)
-    const [pin,setPin]=useState("");
-    const [checkoutData,setCheckoutData]=useState({
-      firstName:"",
-      lastName:"",
-      contact:"",
-      email:"",
-      address:"",
-      address2:"",
-      state:"",
-      city:""
-    })
-    let data1=data?.map(c1=>({totPrice:c1?.MRP*c1?.quantity}));
+  const data = useSelector(state => state.checkoutData)
+  const [pin, setPin] = useState("");
+  const [checkoutData, setCheckoutData] = useState({
+    firstName: "",
+    lastName: "",
+    contact: "",
+    email: "",
+    address: "",
+    address2: "",
+    state: "",
+    city: ""
+  })
+  let data1 = data?.map(c1 => ({ totPrice: c1?.MRP * c1?.quantity }));
 
-    const handleChange=(e)=>{
-        const {currentTarget : input}=e;
-        let checkoutData1={...checkoutData};
-        checkoutData1[input.name]=input.value;
-        setCheckoutData(checkoutData1);
-     }
-    const getStateAndCity=async(pin)=>{
-      try{
-        let response=await axios.get(`https://api.postalpincode.in/pincode/${pin}`);
-        let {data}=response;
-        setCheckoutData({...checkoutData,state:data[0].PostOffice[0].State,city:data[0].PostOffice[0].District});
-      }catch(err){
-        console.log(err);
-      }
+  const handleChange = (e) => {
+    const { currentTarget: input } = e;
+    let checkoutData1 = { ...checkoutData };
+    checkoutData1[input.name] = input.value;
+    setCheckoutData(checkoutData1);
+  }
+  const getStateAndCity = async (pin) => {
+    try {
+      let response = await axios.get(`https://api.postalpincode.in/pincode/${pin}`);
+      let { data } = response;
+      setCheckoutData({ ...checkoutData, state: data[0].PostOffice[0].State, city: data[0].PostOffice[0].District });
+    } catch (err) {
+      console.log(err);
     }
+  }
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -94,12 +94,12 @@ const Checkout = () => {
 
   };
 
-   const handlePin=(e)=>{
-         setPin(e.currentTarget.value)
-         getStateAndCity(e.currentTarget.value);
-   };
+  const handlePin = (e) => {
+    setPin(e.currentTarget.value)
+    getStateAndCity(e.currentTarget.value);
+  };
 
-   let {firstName,lastName,contact,email,address,address2,state,city}=checkoutData;
+  let { firstName, lastName, contact, email, address, address2, state, city } = checkoutData;
   return (
     <>
       <div className='bg-light'>
@@ -115,8 +115,8 @@ const Checkout = () => {
                 <span className="badge badge-primary badge-pill">3</span>
               </h4>
               <ul className="list-group mb-3">
-                {data?.map(d1 =>
-                  <li className="list-group-item d-flex justify-content-between lh-condensed">
+                {data?.map((d1, i) =>
+                  <li key={i} className="list-group-item d-flex justify-content-between lh-condensed">
                     <div>
                       <h6 className="my-0">{d1?.sparePartName}</h6>
                       <small className="text-muted">Quantity : {d1?.quantity}</small>
@@ -220,40 +220,40 @@ const Checkout = () => {
                   </div>
                 </div> */}
                 <div className='row'>
-                 <div className="col-md-6 mb-3">
-                  <label htmlFor="email">
-                    Contact <span className="text-muted"></span>
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="contact"
-                    placeholder=""
-                    name="contact"
-                    value={contact}
-                    onChange={handleChange}
-                  />
-                  <div className="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="email">
+                      Contact No. <span className="text-muted"></span>
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="contact"
+                      placeholder=""
+                      name="contact"
+                      value={contact}
+                      onChange={handleChange}
+                    />
+                    <div className="invalid-feedback">
+                      Please enter a valid email address for shipping updates.
+                    </div>
                   </div>
-                </div>
-                <div className="col-md-6 mb-3">
-                  <label htmlFor="email">
-                    Email <span className="text-muted">(Optional)</span>
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    placeholder="you@example.com"
-                    name="email"
-                    value={email}
-                    onChange={handleChange}
-                  />
-                  <div className="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="email">
+                      Email <span className="text-muted">(Optional)</span>
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="you@example.com"
+                      name="email"
+                      value={email}
+                      onChange={handleChange}
+                    />
+                    <div className="invalid-feedback">
+                      Please enter a valid email address for shipping updates.
+                    </div>
                   </div>
-                </div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="address">Address</label>
@@ -286,7 +286,7 @@ const Checkout = () => {
                   />
                 </div>
                 <div className="row">
-                <div className="col-md-3 mb-3">
+                  <div className="col-md-3 mb-3">
                     <label htmlFor="zip">Pin</label>
                     <input
                       type="text"
@@ -296,7 +296,7 @@ const Checkout = () => {
                       value={pin}
                       placeholder=""
                       required=""
-                      onChange={(e)=>handlePin(e)}
+                      onChange={(e) => handlePin(e)}
                     />
                     <div className="invalid-feedback">Zip code required.</div>
                   </div>
@@ -332,7 +332,7 @@ const Checkout = () => {
                       Please provide a valid state.
                     </div>
                   </div>
-                 
+
                 </div>
                 <hr className="mb-4" />
                 <div className="form-check custom-checkbox">
@@ -398,20 +398,24 @@ const Checkout = () => {
                     <div className="mb-4 text-center">
                       <TaskAltIcon color='success' sx={{ fontSize: "100px" }} />
                     </div>
-                    <div className="ms-5 me-5 text-center">
+                    <div className="ms-2 me-2 text-center">
                       <h1>Thank you for your order!</h1>
                       We’re working hard to get it shipped to you. Hang tight!
                     </div>
-                    
-                    <div className="ms-5 mt-5 me-5">
-                    <h3 className=''>Order Details :</h3>
-                     <div className='border p-2'>  <span className='fw-bold'> OrderId :</span> 62366etyghggyct6ysjhjhgh </div>
-                     <div className='border p-2'>  <span className='fw-bold'> Product Name :</span> fan </div>
-                     <div className='border p-2'>  <span className='fw-bold'> Quantity :</span>  1 </div>
-                     <div className='border p-2'> <span className='fw-bold'> Price :</span> Rs. 500 </div>
+
+                    <div className="ms-5 mt-3 me-5">
+                      <h3 className=''>Order Details :</h3>
+                      {data?.map((d1, i) =>
+                        <div key={i} className='border mb-2 p-3' >
+
+                          <div className=''>  <span className='fw-bold'> OrderId :</span> {d1?.orderId} </div>
+                          <div className=' '>  <span className='fw-bold'> Product Name :</span>  {d1?.sparePartName} </div>
+                          <div className=' '>  <span className='fw-bold'> Quantity :</span>  {d1?.quantity} </div>
+                          <div className=' '> <span className='fw-bold'> Price :</span> Rs. {d1?.MRP * d1?.quantity} </div>
+                        </div >)}
                     </div >
-                    <div  className="text-center mt-3">
-                    <Link href="/" className='text-decoration-none'> <button className="btn btn-primary"> Back to home</button></Link>
+                    <div className="text-center mt-3">
+                      <Link href="/" className='text-decoration-none'> <button className="btn btn-primary"> Back to home</button></Link>
                     </div>
                   </div>
                 </div>

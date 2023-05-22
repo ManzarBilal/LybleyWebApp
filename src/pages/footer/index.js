522b11cfe4d3db38ef48345e2d9a50faa09f962d
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-05-22 17:36:09 +0530

    ;djkghcfggh

--- src/pages/footer/index.js
+++ src/pages/footer/index.js
@@ -16,7 +16,7 @@ export default function Footer() {
                             {/* <p className="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a className="text-primary" href="#">Bootstrapious.com</a></p> */}
                             <div className="mt-md-5 mt-lg-5">
                                <Link href={"https://twitter.com/SpareTradeIN"} target="_blank"><img className="p-1 bg-white rounded-circle" src="https://cdn-icons-png.flaticon.com/512/1051/1051382.png" alt="logo" height="40px" width="40px"  /></Link>  
-                               <Link className="ms-2" href={"https://wwww.linkedin.com/company/94805456/admin"} target="_blank"><img className="p-1 bg-white rounded-circle" src="https://img.icons8.com/?size=512&id=60443&format=png" alt="logo" height="40px" width="40px" /> </Link> 
+                               <Link className="ms-2" href={"https://www.linkedin.com/company/94805456/admin/"} target="_blank"><img className="p-1 bg-white rounded-circle" src="https://img.icons8.com/?size=512&id=60443&format=png" alt="logo" height="40px" width="40px" /> </Link> 
                                 {/* <img className="p-1" src="https://lybley-webapp-collection.s3.amazonaws.com/whatsapp-icon-3933.png-1683869804569-439943255" alt="logo" height="30px" width="30px" /> 
                                 <img className="p-1" src="https://lybley-webapp-collection.s3.amazonaws.com/pngegg.png-1683869876816-914477302" alt="logo" height="30px" width="30px" />  */}
                             </div>

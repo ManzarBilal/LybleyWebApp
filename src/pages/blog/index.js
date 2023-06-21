6b1a2bc88f144d5da7b096cbaad74e559d9f6783
ManzarBilal <manzarbilal@gmail.com>
2023-06-21 14:08:43 +0530

    fg

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,45 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "bootstrap/dist/css/bootstrap.css"
+import httpCommon from '@/http-common'
 
 
 const Blog = () => {
+    const [data,setData]=useState([]);
+
+    useEffect(()=>{
+       getBlogs();
+    },[]);
+
+    const getBlogs=async()=>{
+         try{
+           let response=await httpCommon.get("/getAllBlogs");
+           let {data}=response;
+           setData(data);
+         }catch(err){
+           console.log(err);
+         }
+    }
+
     return (
         <div className='mt-3'>
             <h1> <u> Blog</u></h1>
             <div className="d-flex justify-content-between">
                 <div className="row mt-2">
-                    <div className='col-12 col-md-4 col-lg-4'>
+                   {data?.map(d1=>
+                   <div className='col-12 col-md-4 col-lg-4'>
                         <div className="card">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN6wNytyE66wwTN0ZGUa_-076XQkDi8tjUeAkAvVCBLGNR-XfIyV8blz6pAd5Gi6Pp4cs&usqp=CAU" className="img-fluid" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">Motor</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <h5 className="card-title">{d1?.title}</h5>
+                                <p className="card-text">{d1?.content}</p>
+                                <a href="#" className="btn btn-primary">view</a>
                             </div>
                         </div>
-                    </div>
-                    <div className='col-12 col-md-4 col-lg-4'>
-
-                        <div className="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN6wNytyE66wwTN0ZGUa_-076XQkDi8tjUeAkAvVCBLGNR-XfIyV8blz6pAd5Gi6Pp4cs&usqp=CAU" className="img-fluid" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Motor</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-12 col-md-4 col-lg-4'>
-
-                        <div className="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN6wNytyE66wwTN0ZGUa_-076XQkDi8tjUeAkAvVCBLGNR-XfIyV8blz6pAd5Gi6Pp4cs&usqp=CAU" className="img-fluid" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Motor</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
+                    </div>)}
                 </div>
             </div>
             <h4 className='text-primary mt-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, officiis?</h4>

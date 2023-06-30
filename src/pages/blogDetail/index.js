44486928bf323ccfb73f71d46852442bf97d020b
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-06-30 18:26:20 +0530

    dghgggds'

--- src/pages/blogDetail/index.js
+++ src/pages/blogDetail/index.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router'
 import httpCommon from '@/http-common'
 import DateRangeIcon from '@mui/icons-material/DateRange';
 import ReactLoader from '../loading'
- 
+
 
 
 const BlogDetail = () => {
@@ -18,7 +18,7 @@ const BlogDetail = () => {
 
     useEffect(() => {
         getBlog();
-    }, [ ]);
+    }, []);
 
 
     const getBlog = async () => {
@@ -38,25 +38,30 @@ const BlogDetail = () => {
     return (
         <>
             <Header />
-            {loading===true ? <div  className='vh-100 d-flex align-items-center justify-content-center'><ReactLoader /></div> 
-            :
-            <div className='container'>
-                <div className='mt-5'>
-                    {/* ${style.brandBannerHgt} */}
-                    <img className={` fluid-img rounded`} src={data?.image} alt='' width="100%" />
+            {loading === true ? <div className='vh-100 d-flex align-items-center justify-content-center'><ReactLoader /></div>
+                :
+                <div className='container'>
+                    <div className='mt-5'>
+                        <h2 className='text-break'>{data?.title}</h2>
+                    </div>
+                    <div className='fs-12 d-flex mt-4'>
+                        <DateRangeIcon color='primary' />  <div className='ms-2'>{new Date(data?.createdAt).toDateString()}</div>
+                    </div>
+                    <div className='mt-3'>
+                        <div className='fs-12 text-primary'>{data?.category}</div>
+                    </div>
+                    <div className='mt-5'>
+                        {/* ${style.brandBannerHgt} */}
+                        <img className={`img-fluid rounded`} src={data?.image} alt={data?.metaTitle} width="50%" />
 
+                    </div>
+
+                    <div>
+                        <div style={{fontSize:"16px",fontWeight:"400",color:"#1b1b28",fontFamily:"ColfaxAI, helvetica, sans-serif"}}  className=' mt-5'>{data?.content}</div>
+                    </div>
+                   
                 </div>
-                <div className='mt-5'>
-                    <h2 className='text-break'>{data?.title}</h2>
-                </div>
-                <div>
-                    <h4 className='text-break'>{data?.content}</h4>
-                </div>
-                <div className='d-flex mt-5'>
-                    <DateRangeIcon color='primary' />  <div className='ms-2'>{new Date(data?.createdAt).toDateString()}</div>
-                </div>
-            </div>
-                
+
             }
             <Footer />
         </>

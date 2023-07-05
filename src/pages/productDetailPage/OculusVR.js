369b25196b5b73b94bb823198c5980277a4ddfee
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-07-05 18:09:00 +0530

    dnhggfgd

--- src/pages/productDetailPage/OculusVR.js
+++ src/pages/productDetailPage/OculusVR.js
@@ -18,7 +18,7 @@ function OculusVR(props) {
     }, [getSparePart]);
 
 
-
+ 
     return (
         <div className="col-md-12">
             <div className="card">
@@ -55,7 +55,7 @@ function OculusVR(props) {
                                         <i className="fa fa-star text-warning"></i>
                                         <i className="fa fa-star text-warning"></i>
                                         <i className="fa fa-star text-warning"></i>
-                                        <span className="text-muted ms-3">( <span class="badge bg-primary"> {props?.reviews?.length}</span> customer review )</span>
+                                        <span className="text-muted ms-3">( <span  > {props?.reviews?.length}</span> customer review )</span>
                                     </div>
                                     {product === false && adminProduct?.length > 0 ?
                                     <div className="product-items flex-wrap">
@@ -63,7 +63,7 @@ function OculusVR(props) {
                                         <div className="items-wrapper d-flex" id="select-item-1">
                                             <div className="single-item ">
                                                 <div className="items-image lift">
-                                                    <img src={adminProduct?.images?.filter((img, i) => i === 0)} alt="product"  onClick={() => {  props?.setProduct(true); setMainImage(adminProduct?.images?.filter((img, i) => i === 0)) }}/>
+                                                    <img src={adminProduct?.images?.filter((img, i) => i === 0)} alt={adminProduct?.metaTitle}  onClick={() => {  props?.setProduct(true); setMainImage(adminProduct?.images?.filter((img, i) => i === 0)) }}/>
                                                 </div>
                                                 <p className="text">{adminProduct?.partName}</p>
                                             </div>
@@ -83,16 +83,22 @@ function OculusVR(props) {
                                     </div> */}
                                     <div className="product-price">
                                         <h6 className="price-title fw-bold">Price</h6>
-                                        <p className="sale-price">{getSparePart?.bestPrice} INR <span className='text-muted'> <sub> (18% GST included)</sub></span></p>
+                                        <p className="sale-price">{getSparePart?.bestPrice} INR <span className='text-muted'> <sub>  </sub></span></p>
                                         <p className="regular-price text-danger">{getSparePart?.MRP} INR</p>
                                     </div>
                                     <div className='fw-bold mb-2'>Part Number :  <span className='fw-bold fs-5'>{" "}{getSparePart?.partNo}</span></div>
                                     <div ><span className="fw-bold"> Brand Name :</span>  {getSparePart?.brandName}</div>
                                     <div className='mt-2 mb-3'><span className="fw-bold"> Category Name :</span>  {getSparePart?.category}</div>
-                                    <div>{(getSparePart?.description)?.substr(0,65) } {"..."}</div>
+                                    <div>{(getSparePart?.description)?.substr(0,209)}{"..."} </div>
                                     <div className="product-btn mb-2">
+                                       
+                                        <div className='form-check mb-3'>
+                                            <input type="checkbox" className='form-check-input' value={getSparePart?.technician} checked={technician === 0 ? false : true} onChange={(e) => props?.handleCheckbox(e.currentTarget.value)} />
+                                            <label className='form-check-label'>Book Technician to fit it - {getSparePart?.technician} INR only</label>
+                                        </div>
+                                        <div className='d-flex align-items-center'>
                                         <div className="d-flex flex-wrap">
-                                            <div className="d-flex flex-wrap mb-3 align-items-center">
+                                            <div className="d-flex flex-wrap  ">
                                                 <div className=" mt-sm-0  me-1">
                                                     <div className="">
                                                         <span className='fw-bold me-2'>Qty.</span> <button className='btn btn-outline-danger btn-sm me-2' onClick={() => dispatch(decrement(-1))}>-</button> <span className='text-dark'> {qty} </span> <button className='btn btn-outline-success btn-sm ms-2' onClick={() => dispatch(increment(1))}>+</button>
@@ -104,12 +110,9 @@ function OculusVR(props) {
 
                                             </div>
                                         </div>
-                                        <div className='form-check mb-3'>
-                                            <input type="checkbox" className='form-check-input' value={getSparePart?.technician} checked={technician === 0 ? false : true} onChange={(e) => props?.handleCheckbox(e.currentTarget.value)} />
-                                            <label className='form-check-label'>Book Technician to fit it - {getSparePart?.technician} INR only</label>
+                                        <button className="btn bg-dark text-white mx-3 mt-2  mt-sm-0" onClick={props?.handleBuy}> Buy Now</button>
+                                        <button onClick={(e) => props?.handleAddToCart(getSparePart?._id)} className="btn bg-dark text-white mx-1 mt-2 mt-sm-0 w-sm-100"> Add to Cart</button>
                                         </div>
-                                        <button className="btn btn-primary mx-1 mt-2  mt-sm-0" onClick={props?.handleBuy}> Buy Now</button>
-                                        <button onClick={(e) => props?.handleAddToCart(getSparePart?._id)} className="btn btn-primary mx-1 mt-2 mt-sm-0 w-sm-100"> Add to Cart</button>
 
                                     </div>
 
